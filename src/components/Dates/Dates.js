e04f4310f33a1616fb4ba88428ce1e82661f9f85
Alex Tapia <realalextapia@gmail.com>
2023-04-05 17:36:15 -0400

    frontend routes updated, able to post

--- src/components/Dates/Dates.js
+++ src/components/Dates/Dates.js
@@ -1,190 +1,229 @@
-import React from 'react';
-import '../Dates/Dates.scss';
-import { useEffect, useState } from 'react';
+import React from "react";
+import "../Dates/Dates.scss";
+import { useEffect, useState } from "react";
 import editIcon from "../../assets/edit-24px.svg";
 import deleteIcon from "../../assets/delete_outline-24px.svg";
-import { getDates, postDate } from '../../utils/apiRequest';
+import { getDates, postDate } from "../../utils/apiRequest";
 
 const Dates = (props) => {
-    const [item_id, setItem_id] = useState('');
-    const [data, setData] = useState([]);
+  const [info, setInfo] = useState([]);
+  const [inputFirst, setInputFirst] = useState('');
+  const [inputSecond, setInputSecond] = useState('');
+  const [inputThird, setInputThird] = useState('');
+//   const [input, setInput] = useState();
+
+  const { dateList } = props;
+
+  function handleSubmit(e) {    
+    e.preventDefault();
+
+  const inputArray = [inputFirst,inputSecond,inputThird];
+  const newArray = []
+
+    for(let i =0; i <inputArray.length; i++) {
+      if (inputArray[i]){
+           newArray.push(inputArray[i])
+        } 
+    
+    }
+
+    console.log(newArray)
+
+    const postObj = {
+        daily: newArray,
+    };
+
   
-   
-    const {dateList} = props
 
+    //  post data
+    postDate(postObj);
+  }
 
-        function handleFormSubmit(e) {
-           e.preventDefault();
+  const handleFirstChange = (e) => {
+    e.preventDefault()
+     setInputFirst(e.target.value)
+  }
+  const handleSecondChange = (e) => {
+    e.preventDefault()
+     setInputSecond(e.target.value)
+  }
+  const handleThirdChange = (e) => {
+    e.preventDefault()
+     setInputThird(e.target.value)
+  }
 
-           const form = e.target;
+  //const value = e.target.value; 
+  //setInput({...input,[e.target.value]:value});
 
-           const item_id = form.item_id.value;
-           const task = form.data.value;
-           console.log(form)
-           console.log(task)
-           //    const inputvalues =  {
-               //     // form.category.value,
-               //     data:data
-               //     }   
-               //    console.log(inputvalues)
-               console.log(form.category)
-               console.log(form.data)
-               
-            }
-            
-            
-        //  if (!item_id || !task) {
-        //     //alerts are bad going to get ride of this
-        //     // alert('skip to next day ')
-        //     return true;
-        // }
-
-         //get date
-         useEffect(() =>{
-            getDates()
-            .then(res => {
-            console.log(res)
-            setData(res)
-            }).catch(error => console.log(error))
-        },[]);
-            
-        console.log(data)
-        //post data
-            postDate() 
-              .then((res) =>{
-            })
-            
+  //get date
+  useEffect(() => {
+    getDates()
+      .then((res) => {
+        setInfo(res);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
 
 
-    return (
-        <div className='date'>
-            <form onSubmit={handleFormSubmit} className='date__form'>
-              
-                 <div className='date__form--title'>
-                            <label className='date__form--day'>Monday</label>
-                            <div className='date__form--info'>
-                            <input className='date__form--details' type="text" placeholder = {!(data.length===0) ? data[0].data : '' }
-                            onChange={(e) => data(e.target.value)}/>
-                            <input className='date__form--details' type="text" value = {data.data} 
-                            onChange={(e) => setData(e.target.value)}/>
-                            <input className='date__form--details' type="text" value = {data.data}
-                            onChange={(e) => setData(e.target.value)} />
-                            <input className='date__form--details' type="text" value = {data.data}
-                            onChange={(e) => setData(e.target.value)} />
-                            <input className='date__form--details' type="text" value ={data.data}
-                            onChange={(e) => setData(e.target.value)} />
-                            </div>
-                            <div className='btn'>
-                            <button className='btn__submit' type='text' onChange={(e) => setData(e.target.value)}> Submit </button>
-                            <img src={editIcon} alt='editIcon' type='text'/>
-                            <img src={deleteIcon} alt='deleteIcon'/>
-                            </div>
-                        </div>
-
-                <div className='date__form--title'>
-                            <label className='date__form--day'>Tuesday</label>
-                            <div className='date__form--info'>
-                            <input className='date__form--details' type="text"
-                            onChange={(e) => setData(e.target.value)}/>
-                            <input className='date__form--details' type="text" name ="details" />
-                            <input className='date__form--details' type="text" name ="details" />
-                            <input className='date__form--details' type="text" name ="details" />
-                            <input className='date__form--details' type="text" name ="details" />                            </div>
-                            <div className='btn'>
-                            <button className='btn__submit' type='text'> Submit </button>
-                            <img src={editIcon} alt='editIcon'/>
-                            <img src={deleteIcon} alt='deleteIcon'/>
-                            </div>
-                        </div>
-
-                
-                 <div className='date__form--title'>
-                            <label className='date__form--day'>Wednesday</label>
-                            <div className='date__form--info'>
-                            <input className='date__form--details' type="text" name ="data" 
-                            onChange={(e) => setData(e.target.value)}/>
-                            <input className='date__form--details' type="text" name ="details" />
-                            <input className='date__form--details' type="text" name ="details" />
-                            <input className='note__form--details' type="text" name ="details" />
-                            <input className='note__form--details' type="text" name ="details" />
-                            </div>
-                            <div className='btn'>
-                            <button className='btn__submit' type='text'> Submit </button>
-                            <img src={editIcon} alt='editIcon'/>
-                            <img src={deleteIcon} alt='deleteIcon'/>
-                            </div>
-                        </div>
-
-                   
-                        <div className='date__form--title'>
-                            <label className='date__form--day'>Thursday</label>
-                            <div className='date__form--info'>
-                            <input className='date__form--details' type="text" name ="details" />
-                            <input className='date__form--details' type="text" name ="details" />
-                            <input className='date__form--details' type="text" name ="details" />
-                            <input className='note__form--details' type="text" name ="details" />
-                            <input className='note__form--details' type="text" name ="details" />
-                            </div>
-                            <div className='btn'>
-                            <button className='btn__submit' type='text'> Submit </button>
-                            <img src={editIcon} alt='editIcon'/>
-                            <img src={deleteIcon} alt='deleteIcon'/>
-                            </div>
-                        </div>
-
-                        <div className='date__form--title'>
-                            <label className='date__form--day'>Friday</label>
-                            <div className='date__form--info'>
-                            <input className='date__form--details' type="text" name ="details" />
-                            <input className='date__form--details' type="text" name ="details" />
-                            <input className='date__form--details' type="text" name ="details" />
-                            <input className='note__form--details' type="text" name ="details" />
-                            <input className='note__form--details' type="text" name ="details" />
-                            </div>
-                            <div className='btn'>
-                            <button className='btn__submit' type='text'> Submit </button>
-                            <img src={editIcon} alt='editIcon'/>
-                            <img src={deleteIcon} alt='deleteIcon'/>
-                            </div>
-                        </div>
-
-                        <div className='date__form--title'>
-                            <label className='date__form--day'>Saturday</label>
-                            <div className='date__form--info'>
-                            <input className='date__form--details' type="text" name ="details" />
-                            <input className='date__form--details' type="text" name ="details" />
-                            <input className='date__form--details' type="text" name ="details" />
-                            <input className='note__form--details' type="text" name ="details" />
-                            <input className='note__form--details' type="text" name ="details" />
-                            </div>
-                            <div className='btn'>
-                            <button className='btn__submit' type='text'> Submit </button>
-                            <img src={editIcon} alt='editIcon'/>
-                            <img src={deleteIcon} alt='deleteIcon'/>
-                            </div>
-                        </div>
-
-                        <div className='date__form--title'>
-                            <label className='date__form--day'>Sunday</label>
-                            <div className='date__form--info'>
-                            <input className='date__form--details' type="text" name ="details" />
-                            <input className='date__form--details' type="text" name ="details" />
-                            <input className='date__form--details' type="text" name ="details" />
-                            <input className='note__form--details' type="text" name ="details" />
-                            <input className='note__form--details' type="text" name ="details" />
-                            </div>
-                            <div className='btn'>
-                            <button className='btn__submit' type='text'> Submit </button>
-                            <img src={editIcon} alt='editIcon'/>
-                            <img src={deleteIcon} alt='deleteIcon'/>
-                            </div>
-                        </div>
-                    
-                    </form>   
-            
+  return (
+    <div className="date">
+      <form onSubmit={handleSubmit} className="date__form">
+        <div className="date__form--title">
+          <label className="date__form--day">Monday</label>
+          <div className="date__form--info">
+            <input
+              className="date__form--details"
+              type="text"
+            //   placeholder={info.length !== 0 ? info[0].info : ""}
+              onChange={handleFirstChange}
+              value={inputFirst}
+            />
+            <input
+              className="date__form--details"
+              type="text"
+              name="data"
+              onChange={handleSecondChange}
+              value={inputSecond}
+            />
+            <input 
+              className="date__form--details" 
+              type="text" 
+              name="data" 
+              onChange={handleThirdChange}
+              value={inputThird}/>
+           
+          </div>
+          <div className="btn">
+            <button className="btn__submit" type="submit">
+              {" "}
+              Submit{" "}
+            </button>
+            <img src={editIcon} alt="editIcon" type="text" />
+            <img src={deleteIcon} alt="deleteIcon" />
+          </div>
         </div>
-    );
+
+        <div className="date__form--title">
+          <label className="date__form--day">Tuesday</label>
+          <div className="date__form--info">
+            <input className="date__form--details" type="text" />
+            <input className="date__form--details" type="text" name="details" />
+            <input className="date__form--details" type="text" name="details" />
+            <input className="date__form--details" type="text" name="details" />
+            <input
+              className="date__form--details"
+              type="text"
+              name="details"
+            />{" "}
+          </div>
+          <div className="btn">
+            <button className="btn__submit" type="text">
+              {" "}
+              Submit{" "}
+            </button>
+            <img src={editIcon} alt="editIcon" />
+            <img src={deleteIcon} alt="deleteIcon" />
+          </div>
+        </div>
+
+        <div className="date__form--title">
+          <label className="date__form--day">Wednesday</label>
+          <div className="date__form--info">
+            <input className="date__form--details" type="text" name="data" />
+            <input className="date__form--details" type="text" name="details" />
+            <input className="date__form--details" type="text" name="details" />
+            <input className="note__form--details" type="text" name="details" />
+            <input className="note__form--details" type="text" name="details" />
+          </div>
+          <div className="btn">
+            <button className="btn__submit" type="text">
+              {" "}
+              Submit{" "}
+            </button>
+            <img src={editIcon} alt="editIcon" />
+            <img src={deleteIcon} alt="deleteIcon" />
+          </div>
+        </div>
+
+        <div className="date__form--title">
+          <label className="date__form--day">Thursday</label>
+          <div className="date__form--info">
+            <input className="date__form--details" type="text" name="details" />
+            <input className="date__form--details" type="text" name="details" />
+            <input className="date__form--details" type="text" name="details" />
+            <input className="note__form--details" type="text" name="details" />
+            <input className="note__form--details" type="text" name="details" />
+          </div>
+          <div className="btn">
+            <button className="btn__submit" type="text">
+              {" "}
+              Submit{" "}
+            </button>
+            <img src={editIcon} alt="editIcon" />
+            <img src={deleteIcon} alt="deleteIcon" />
+          </div>
+        </div>
+
+        <div className="date__form--title">
+          <label className="date__form--day">Friday</label>
+          <div className="date__form--info">
+            <input className="date__form--details" type="text" name="details" />
+            <input className="date__form--details" type="text" name="details" />
+            <input className="date__form--details" type="text" name="details" />
+            <input className="note__form--details" type="text" name="details" />
+            <input className="note__form--details" type="text" name="details" />
+          </div>
+          <div className="btn">
+            <button className="btn__submit" type="text">
+              {" "}
+              Submit{" "}
+            </button>
+            <img src={editIcon} alt="editIcon" />
+            <img src={deleteIcon} alt="deleteIcon" />
+          </div>
+        </div>
+
+        <div className="date__form--title">
+          <label className="date__form--day">Saturday</label>
+          <div className="date__form--info">
+            <input className="date__form--details" type="text" name="details" />
+            <input className="date__form--details" type="text" name="details" />
+            <input className="date__form--details" type="text" name="details" />
+            <input className="note__form--details" type="text" name="details" />
+            <input className="note__form--details" type="text" name="details" />
+          </div>
+          <div className="btn">
+            <button className="btn__submit" type="text">
+              {" "}
+              Submit{" "}
+            </button>
+            <img src={editIcon} alt="editIcon" />
+            <img src={deleteIcon} alt="deleteIcon" />
+          </div>
+        </div>
+
+        <div className="date__form--title">
+          <label className="date__form--day">Sunday</label>
+          <div className="date__form--info">
+            <input className="date__form--details" type="text" name="details" />
+            <input className="date__form--details" type="text" name="details" />
+            <input className="date__form--details" type="text" name="details" />
+            <input className="note__form--details" type="text" name="details" />
+            <input className="note__form--details" type="text" name="details" />
+          </div>
+          <div className="btn">
+            <button className="btn__submit" type="text">
+              {" "}
+              Submit{" "}
+            </button>
+            <img src={editIcon} alt="editIcon" />
+            <img src={deleteIcon} alt="deleteIcon" />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Dates;
